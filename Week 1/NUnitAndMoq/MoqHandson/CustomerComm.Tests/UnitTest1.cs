45c81653af6c17861597a32595805fd7d5d5bd03
Ayush Raj <your-ayushmaan1807@gmail.com>
2026-07-03 01:40:39 +0530

    Completed Moq Handson

---
+++ Week 1/NUnitAndMoq/MoqHandson/CustomerComm.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerCommTests
+{
+    public class Tests
+    {
+        [Test]
+        public void SendMailToCustomer_ReturnsTrue()
+        {
+            // Arrange
+            var mockMailSender = new Mock<IMailSender>();
+
+            mockMailSender
+                .Setup(x => x.SendMail(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(true);
+
+            var customerComm = new CustomerCommLib.CustomerComm(mockMailSender.Object);
+
+            // Act
+            bool result = customerComm.SendMailToCustomer();
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ Week 1/NUnitAndMoq/MoqHandson/CustomerCommLib/CustomerComm.cs
@@ -0,0 +1,22 @@
+namespace CustomerCommLib
+{
+    public class CustomerComm
+    {
+        private readonly IMailSender _mailSender;
+
+        public CustomerComm(IMailSender mailSender)
+        {
+            _mailSender = mailSender;
+        }
+
+        public bool SendMailToCustomer()
+        {
+            string toAddress = "cust123@abc.com";
+            string message = "Some Message";
+
+            _mailSender.SendMail(toAddress, message);
+
+            return true;
+        }
+    }
+}
