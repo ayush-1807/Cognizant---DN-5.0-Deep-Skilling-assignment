196dbaa7dc56bbbc94387d2e8bf2ab2833fe8841
Ayush Raj <your-ayushmaan1807@gmail.com>
2026-07-27 00:12:28 +0530

    Completed Angular Hands-on 5

--- Week6/student-course-portal/src/app/app.routes.ts
+++ Week6/student-course-portal/src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
+import { ReactiveEnrollmentForm } from './pages/reactive-enrollment-form/reactive-enrollment-form';
 
 export const routes: Routes = [
-  {
-    path: 'enroll',
-    component: EnrollmentForm
-  },
-  {
-    path: '',
-    redirectTo: 'enroll',
-    pathMatch: 'full'
-  }
+  { path: 'enroll', component: EnrollmentForm },
+  { path: 'enroll-reactive', component: ReactiveEnrollmentForm },
+  { path: '', redirectTo: 'enroll-reactive', pathMatch: 'full' }
 ];
